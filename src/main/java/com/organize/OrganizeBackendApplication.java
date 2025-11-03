795e2d6ea7d373cd0586e7519313375e5b8220f6
nielassis <nielvitorba@gmail.com>
2025-11-03 17:33:31 -0300

    chore: allows patch requisitions

--- src/main/java/com/organize/OrganizeBackendApplication.java
+++ src/main/java/com/organize/OrganizeBackendApplication.java
@@ -20,7 +20,7 @@ public class OrganizeBackendApplication {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOriginPatterns("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
